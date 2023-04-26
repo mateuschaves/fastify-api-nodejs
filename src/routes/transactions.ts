72d06a2ff3b6e3e71afb970066814b8e4eb433ec
Mateus Henrique <mateushenriquechaves1@gmail.com>
2023-04-26 12:05:46 -0300

    feat: list transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,29 @@ import { knex } from '../database'
 import { z } from 'zod'
 
 export async function transactionRoutes(app: FastifyInstance) {
+  app.get('/', async (request, reply) => {
+    const transactions = await knex('transactions').select('*')
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const params = getTransactionParamsSchema.parse(request.params)
+
+    const { id } = params
+
+    const transaction = await knex('transactions')
+      .select('*')
+      .where({ id })
+      .first()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
